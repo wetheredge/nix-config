55190c7597e864c13b21cb7d0f98fe6688ccbccd
Wren Etheredge <me@wetheredge.com>
2025-07-27 17:37:55 -0400

    Set up VCS config

--- home/dev/vcs.nix
+++ home/dev/vcs.nix
@@ -1,9 +1,61 @@
-{ ... }: {
+{ ... }:
+let
+  name = "Wren Etheredge";
+  email = "me@wetheredge.com";
+in {
   programs.git = {
     enable = true;
-    userName = "Wren Etheredge";
-    userEmail = "me@wetheredge.com";
+    userName = name;
+    userEmail = email;
+
+    # Also used by jujutsu
+    ignores = [ "*~" ];
+
+    aliases = {
+      tree = "log --graph --decorate --pretty=oneline --abbrev-commit --all";
+    };
+
+    extraConfig = {
+      diff.renames = "copies";
+      fetch.pruneTags = true;
+      help.autoCorrect = "prompt";
+      init.defaultBranch = "main";
+      log.date = "human";
+      merge.conflictStyle = "diff3";
+      rebase.autoSquash = true;
+      rebase.missingCommitsCheck = "error";
+      stash.showPatch = true;
+      status.showStash = true;
+      tag.gpgSign = true;
+    };
   };
 
-  # TODO: jujutsu
+  # programs.git.delta = {
+  #   enable = true;
+  # };
+
+  programs.jujutsu = {
+    enable = true;
+    settings = {
+      user = {
+        inherit name email;
+      };
+
+      git = {
+        push-bookmark-prefix = "wetheredge/push-";
+        # Automatically track newly discovered remote bookmarks
+        auto-local-bookmark = true;
+      };
+    };
+  };
+
+  programs.gh = {
+    enable = true;
+    settings = {
+      git_protocol = "ssh";
+    };
+    hosts."github.com" = {
+      user = "wetheredge";
+    };
+  };
 }
