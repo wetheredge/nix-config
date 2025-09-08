e49b4af20bd803506be110e62ee28738657c5ee2
Wren Etheredge <me@wetheredge.com>
2025-09-08 17:12:03 -0400

    home(desktop):firefox: Add more search engines

--- home/desktop/firefox.nix
+++ home/desktop/firefox.nix
@@ -97,30 +97,68 @@
         engines = let
           channel = osConfig.system.nixos.release;
           nixSnowflake = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-          nixSearch = aliases: url: params: {
+          rustLogo = "https://www.rust-lang.org/static/images/favicon.svg";
+          mkUrl = template: terms: extraParams: {
+            inherit template;
+            params =
+              lib.mapAttrsToList
+              (name: value: {inherit name value;})
+              ({"${terms}" = "{searchTerms}";} // extraParams);
+          };
+          searchNixosOrg = aliases: type: {
             definedAliases = aliases;
             icon = nixSnowflake;
-            urls = [
-              {
-                template = url;
-                params = lib.mapAttrsToList (name: value: {inherit name value;}) params;
-              }
-            ];
+            urls = [(mkUrl "https://search.nixos.org/${type}" "query" {inherit channel;})];
           };
-          searchNixosOrg = aliases: type:
-            nixSearch aliases "https://search.nixos.org/${type}" {
-              inherit channel;
-              query = "{searchTerms}";
-            };
+          rustdoc = crate: {
+            definedAliases = ["@${crate}.rs"];
+            icon = rustLogo;
+            urls = [(mkUrl "https://doc.rust-lang.org/nightly/std/" "search" {})];
+          };
         in {
           "Nix Packages" = searchNixosOrg ["@nixpkgs" "@np"] "packages";
           "Nix Options" = searchNixosOrg ["@no"] "options";
-          "NixOS Wiki" = nixSearch ["@nw"] "https://wiki.nixos.org/w/index.php" {
-            search = "{searchTerms}";
+          "NixOS Wiki" = {
+            definedAliases = ["@nw"];
+            icon = nixSnowflake;
+            urls = [(mkUrl "https://wiki.nixos.org/w/index.php" "search" {})];
           };
-          "Home Manager Options" = nixSearch ["@ho"] "https://home-manager-options.extranix.com" {
-            release = "release-${channel}";
-            query = "{searchTerms}";
+          "Home Manager Options" = {
+            definedAliases = ["@ho"];
+            icon = nixSnowflake;
+            urls = [(mkUrl "https://home-manager-options.extranix.com" "query" {release = "release-${channel}";})];
+          };
+          "MDN" = {
+            definedAliases = ["@mdn"];
+            icon = "https://developer.mozilla.org/favicon.svg";
+            urls = [(mkUrl "https://developer.mozilla.org/search" "q" {})];
+          };
+          "Rust crates (lib.rs)" = {
+            definedAliases = ["@rs" "@lib.rs"];
+            icon = "https://lib.rs/crates-logo.png";
+            urls = [(mkUrl "https://lib.rs/search" "q" {})];
+          };
+          "Rust crates (crates.io)" = {
+            definedAliases = ["@crates"];
+            icon = "https://crates.io/assets/cargo.png";
+            urls = [(mkUrl "https://crates.io/search" "q" {})];
+          };
+          "Rust crate docs" = {
+            icon = rustLogo;
+            urls = [(mkUrl "https://docs.rs/releases/search" "query" {})];
+          };
+          "Rust std docs" = rustdoc "std";
+          "Rust alloc docs" = rustdoc "alloc";
+          "Rust core docs" = rustdoc "core";
+          "NPM Packages" = {
+            definedAliases = ["@npm"];
+            icon = "https://static-production.npmjs.com/1996fcfdf7ca81ea795f67f093d7f449.png";
+            urls = [(mkUrl "https://www.npmjs.com/search" "q" {})];
+          };
+          "Wikipedia" = {
+            definedAliases = ["@wiki"];
+            icon = "https://www.wikipedia.org/static/favicon/wikipedia.ico";
+            urls = [(mkUrl "https://www.wikipedia.org/search-redirect.php" "search" {})];
           };
         };
       };
