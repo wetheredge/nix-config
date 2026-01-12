24b33bddcbdb329b596fd14e41cdf53624efb6e0
Wren Etheredge <me@wetheredge.com>
2026-01-12 15:36:26 -0500

    host(shelob): Preserve postgresql data

--- hosts/shelob/default.nix
+++ hosts/shelob/default.nix
@@ -1,4 +1,8 @@
-{modulesPath, ...}: {
+{
+  config,
+  modulesPath,
+  ...
+}: {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
     (modulesPath + "/profiles/qemu-guest.nix")
@@ -15,6 +19,17 @@
     enable = true;
     device = "/dev/disk/by-partlabel/disk-main-root";
   };
+
+  preservation.preserveAt.data.directories = let
+    psql = config.systemd.services.postgresql.serviceConfig;
+  in [
+    {
+      directory = "/var/lib/postgresql";
+      user = psql.User;
+      group = psql.Group;
+      mode = "0750";
+    }
+  ];
 
   system.stateVersion = "25.05";
 }
